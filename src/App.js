24b79ae4028e6a645faffc32aa11d4c87a802a5b
Emma Lytle <Emma@tenacious3.com>
2021-09-26 09:23:32 -0600

    updated
    testing

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import BudgetCalculator from './BudgetCalculator.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Budget Calculator...</h1>
+      </header>
+      <BudgetCalculator />
+    </div>
+
+  );
+}
+
+export default App;
