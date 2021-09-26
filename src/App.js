b2f12e10ecdcc8825454cd50d0163c9d8250f60a
Emma Lytle <54733100+EmmaLytle@users.noreply.github.com>
2021-09-26 10:41:54 -0700

    Test action

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Budget Calculator..</h1>
+      <h1>Budget Calculator....</h1>
       </header>
       <BudgetCalculator />
     </div>
