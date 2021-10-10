98af7f944cde1268670bb1cae0536b3dce221615
Emma Lytle <54733100+EmmaLytle@users.noreply.github.com>
2021-10-10 14:56:08 -0700

    Drop down test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Dropdown from './components/Dropdown';
 import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className='todo-app'>
       <TodoList />
+      <Dropdown />
     </div>
   );
 }
---
+++ src/components/Dropdown.js
@@ -0,0 +1,62 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Dropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "coconut",
+      countries: [
+        { id: "1", country: "Cambodia" }, 
+        { id: "2", country: "Australia" },
+        { id: "3", country: "US" }
+      ]
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    alert("Your favorite flavor is: " + this.state.value);
+    event.preventDefault();
+  }
+
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <br />
+        <br />
+        <label>
+          Looping through Array
+          <select>
+            {this.state.countries.map(item => (
+              <option key={item.id} value={item.country}>
+                {item.country}
+              </option>
+            ))}
+            {console.log(this.state.countries)}
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+
+export default Dropdown;
+
+
